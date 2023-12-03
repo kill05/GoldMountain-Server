3ace813046fcc7013c94690cc5d9d457e5d4c1ec
kill05 <vinkimeme@libero.it>
2023-12-03 23:20:53 +0100

    Turned GMServer into a non-singleton class

--- src/main/java/com/github/kill05/goldmountain/GMServer.java
+++ src/main/java/com/github/kill05/goldmountain/GMServer.java
@@ -1,30 +1,26 @@
 package com.github.kill05.goldmountain;
 
 import com.github.kill05.goldmountain.commands.CommandHandler;
+import com.github.kill05.goldmountain.player.PlayerCostume;
 import com.github.kill05.goldmountain.protocol.ServerConnection;
-import com.github.kill05.goldmountain.protocol.packets.TestPacket;
+import com.github.kill05.goldmountain.protocol.packets.io.PacketInOutPlayerUpdate;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joml.Vector2f;
 
 public class GMServer {
 
     public static final int TARGET_TPS = 20;
     public static final Logger logger = LogManager.getLogger("Server");
 
-    private static GMServer instance;
     private Thread serverThread;
     private ServerConnection serverConnection;
     private CommandHandler commandHandler;
     private int currentTick;
     private float tps;
 
-    private GMServer() {
+    public GMServer() {
         init();
-    }
-
-
-    public static GMServer instance() {
-        return instance != null ? instance : (instance = new GMServer());
     }
 
 
@@ -75,9 +71,40 @@ public class GMServer {
 
     private void tick() {
         commandHandler.processInput();
-        //if (currentTick % 2 == 0) serverConnection.sendPacket(new TestPacket("5d00000001feff69de5900cc008c00cc008c00cc008c00cc008c002d00ffffffff9700ffffffff00000000e8030dcd4100000101000c000000910098009100980091009800910098001400ffffffff0000ffffffff00000000e803"));
-        //if (currentTick % 2 == 1) serverConnection.sendPacket(new TestPacket("5d00000001feff69fe5900cf008c00cc008c00cc008c00cc008c002d00ffffffff9800ffffffff00000000e8030dcd4100000101000c000010910098009100980091009800910098001400ffffffff9900ffffffff00000000e803"));
+
+        /*
+        for(int i = 1; i < 11; i++) {
+            sendTestPlayer((short) i, new Vector2f(-0.5f + i, 0.5f), PlayerCostume.values()[i], (short) 0x1400);
+        }
+        */
+
+        //serverConnection.sendPacket(new TestPacket("05 a007 c000 5f00 fb00 0000 0580 0080 0061 0000 00000006 8006 a003 0400 0680 03a0 0604 00"));
+        //serverConnection.sendPacket(new TestPacket("05 a007 c000 5f00 fb00 0000 0580 0780 0761 0000 00000006 8006 a003 0400 0680 03a0 0604 00"));
+        //serverConnection.sendPacket(new TestPacket("03 4700 0000 0000 2b00 0000 0101 007e 0000  00c70253 02c70253 02c70253 02c70253 0214 003e  0000  00000047 9700 00000000 00e8 03"));
+        //serverConnection.sendPacket(new TestPacket("01feff69de5900cc008c00cc008c00cc008c00cc008c002d00ffffffff9700ffffffff00000000e8030dcd4100000101000c000000910098009100980091009800910098001400ffffffff0000ffffffff00000000e803"));
         //serverConnection.sendPacket(new PacketOutServerPlayerUpdate());
+    }
+
+    public void sendTestPlayer(short id, Vector2f loc, PlayerCostume costume, short yaw) {
+        PacketInOutPlayerUpdate packet = new PacketInOutPlayerUpdate();
+
+        packet.setPlayerId(id);
+        packet.setMetadata(0x78000000);
+
+        packet.setCostume(costume);
+        packet.setClock((byte) 0x00);
+        packet.setCurrentAction(0xffff_ffff);
+        packet.setUnknown_1(0xffff_ffff);
+
+        packet.setYaw(yaw);
+        packet.setUnknown_2((short) 0x4300);
+
+        packet.setCurrentPos(loc);
+        packet.setFuturePos(loc);
+        packet.setLocation3(loc);
+        packet.setLocation4(loc);
+
+        serverConnection.sendPacket(packet);
     }
 
 
@@ -87,11 +114,15 @@ public class GMServer {
     }
 
 
+    public Thread getServerThread() {
+        return serverThread;
+    }
+
     public ServerConnection getConnection() {
         return serverConnection;
     }
 
-    public CommandHandler getCommandReader() {
+    public CommandHandler getCommandHandler() {
         return commandHandler;
     }
 
--- src/main/java/com/github/kill05/goldmountain/Main.java
+++ src/main/java/com/github/kill05/goldmountain/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Configurator.setLevel("io.netty", Level.INFO);
-        GMServer.instance();
+        new GMServer();
     }
 
 }
