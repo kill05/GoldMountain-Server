b9c5d3496a3e84b3d8c21744b40ffb84eb888766
kill05 <vinkimeme@libero.it>
2023-12-03 17:45:37 +0100

    added vecToString

--- src/main/java/com/github/kill05/goldmountain/utils/Utils.java
+++ src/main/java/com/github/kill05/goldmountain/utils/Utils.java
@@ -1,9 +1,15 @@
 package com.github.kill05.goldmountain.utils;
 
+import org.joml.Vector2f;
+
 public class Utils {
 
     public static String insertString(String str, String insert, int position) {
         return str.substring(0, position) + insert + str.substring(position);
+    }
+
+    public static String vecToString(Vector2f vec) {
+        return String.format("x: %s, y: %s", vec.x, vec.y);
     }
 
 }
