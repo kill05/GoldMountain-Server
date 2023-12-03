b0165abc34a63ad6f89c50b508147b8378a80d6b
kill05 <vinkimeme@libero.it>
2023-12-03 21:52:37 +0100

    Changed TeleportCommand to only accept hex values

--- src/main/java/com/github/kill05/goldmountain/commands/registered/TeleportCommand.java
+++ src/main/java/com/github/kill05/goldmountain/commands/registered/TeleportCommand.java
@@ -4,6 +4,8 @@ import com.github.kill05.goldmountain.GMServer;
 import com.github.kill05.goldmountain.commands.Command;
 import com.github.kill05.goldmountain.commands.senders.CommandSender;
 import com.github.kill05.goldmountain.dimension.DimensionType;
+import com.github.kill05.goldmountain.protocol.ServerConnection;
+import com.github.kill05.goldmountain.protocol.packets.TestPacket;
 import com.github.kill05.goldmountain.protocol.packets.out.PacketOutDimension;
 import org.apache.commons.lang3.EnumUtils;
 
@@ -29,12 +31,8 @@ public class TeleportCommand extends Command {
             id = type.getId();
         } else {
             try {
-                id = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                try {
-                    id = Integer.decode(args[0]);
-                } catch (NumberFormatException ignored) {
-                }
+                id = Integer.decode(args[0].startsWith("0x") ? args[0] : "0x" + args[0]);
+            } catch (NumberFormatException ignored) {
             }
 
         }
@@ -44,7 +42,11 @@ public class TeleportCommand extends Command {
             return;
         }
 
-        server.getConnection().sendPacket(new PacketOutDimension(id.byteValue()));
+        ServerConnection connection = server.getConnection();
+        connection.sendPacket(new PacketOutDimension(id.byteValue()));
+        connection.sendPacket(new TestPacket("05 a007 c000 5f00 fb00 0000 0580 0080 0061 0000 00000006 8006 a003 0400 0680 03a0 0604 00"));
+        connection.sendPacket(new TestPacket("05 a007 c000 5f00 fb00 0000 0580 0080 0061 0000 00000006 8006 a003 0400 0680 03a0 0604 00"));
+
         sender.sendMessage(String.format("Teleported to dimension '%s'.", id));
     }
 
